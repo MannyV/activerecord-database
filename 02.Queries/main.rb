364d50c9021f5c6f38b6704135373b8c48b4591b
MannyV <evayleux@gmail.com>
2014-01-27 17:29:03 +0100

    02

--- 02.Queries/main.rb
+++ 02.Queries/main.rb
@@ -5,15 +5,14 @@ database_path = "db/jukebox.sqlite"
 db = SQLite3::Database.new(database_path)
 
 # 1. returns the list of tracks with their album and artist
-tracks = # your code here
-
+tracks = db.execute ("SELECT Track.Name, Artist.Name, Album.Title FROM Album INNER JOIN Track ON Track.AlbumId = Album.AlbumId INNER JOIN Artist ON Artist.ArtistId = Album.ArtistId;")
 
 # 2. For each genre of music, finds the number of tracks and the average song length.
-# your code here.
+genre_count = db.execute("SELECT GenreId, COUNT(*), AVG(Milliseconds) FROM Track GROUP BY GenreId")
 
 
 # 3. List the top 5 rock artists
-# your code here.
+top_five = db.execute("SELECT artist.name, count(track.trackId) FROM track INNER JOIN album ON track.albumid = album.albumid INNER JOIN artist ON artist.artistid = album.artistid INNER JOIN genre ON genre.genreid = track.genreid WHERE genre.name == 'Rock' GROUP BY artist.name ORDER BY track.trackid DESC limit 5")
 
 # sql = " SELECT a.name, count(t.trackId) FROM Track t inner join Album b on t.albumId = b.albumId "
 # sql += " inner join Artist a on a.artistId = b.artistId "
